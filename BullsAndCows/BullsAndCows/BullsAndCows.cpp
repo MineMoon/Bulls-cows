bfac9edd80607dd347844c370ba4e9e20b7fe634
MineMoon <mostmiha00@gmail.com>
2024-12-10 18:13:58 +0500

    Добавление функции генерации числа и подсчета быков и коров, создание модуля и head файла

--- BullsAndCows/BullsAndCows/BullsAndCows.cpp
+++ BullsAndCows/BullsAndCows/BullsAndCows.cpp
@@ -1,66 +1,26 @@
 ﻿#include <string>
 #include <iostream>
+#include <random>
+#include <cstdlib>
+#include <time.h>
 #include <set>
 #include<windows.h>
-
-int check_input(std::string s) {
-	std::set<char> numbers;
-	for (int i = 0; i < s.size(); i++) {
-		if ((int)s[i] < 48 || (int)s[i] > 57) {
-			return 1;
-		}
-		numbers.insert(s[i]);
-	}
-	if (s.size() != 4) {
-		return 3;
-	}
-	if (s[0] == '0') {
-		return 2;
-	}
-	if (numbers.size() != 4) {
-		return 4;
-	}
-	return 0;
-}
-
-void countBullsAndCows(std::string secret, std::string guess, int& bulls, int& cows) {
-	bulls = 0;
-	cows = 0;
-
-	//Подсчет быков
-	for (int i = 0; i < secret.length(); i++) {
-		if (guess[i] == secret[i]) {
-			bulls++;
-		}
-	}
-
-	//Подсчет коров
-	for (int i = 0; i < secret.length(); i++) {
-		for (int j = 0; j < secret.length(); j++) {
-			if (i != j && guess[i] == secret[j]) {
-				cows++;
-			}
-		}
-	}
-}
-
+#include"add.h";
 int main() {
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
-	int length = 4; // Длина загаданного числа
-	/*std::string number = generateNumber(length);*/ //Сделать функцию генерации 4-х значного числа, состоящего из уникальных чисел, возращать в int
+	std::string number = Random();
 	std::cout << "Добро пожаловать в игру 'Быки и коровы'!" << std::endl;
-	std::cout << "Компьютер загадал число из " << length << " уникальных цифр." << std::endl;
+	std::cout << "Компьютер загадал число из 4 уникальных цифр." << std::endl;
 	std::cout << "Ваша задача - угадать это число." << std::endl;
 	std::cout << "Введите ваше предположение, и вам сообщат количество быков и коров." << std::endl;
 	std::cout << "Быки - цифра на правильном месте, коровы - цифра есть, но на неправильном месте." << std::endl;
 
 	std::string guess;
 	int bulls = 0, cows = 0;
-	int attempts = 0;
-
-	// Основной игровой цикл
-	while (bulls != length) {
+	int attempts = 0; // счетчик
+	std::cout << std::endl;
+	while (bulls != 4) {
 		std::cout << "Введите ваше предположение: ";
 		std::cin >> guess; 
 
@@ -87,11 +47,11 @@ int main() {
 		}
 
 		++attempts;
-		countBullsAndCows(secret, guess, bulls, cows); //Функция подсчета коров и быков
+		countBullsAndCows(number, guess, bulls, cows); //Функция подсчета коров и быков
 		std::cout << "Быки: " << bulls << ", Коровы: " << cows << std::endl;
 
-		if (bulls == length) {
-			std::cout << "поздравляем! вы угадали число " << number << " за " << attempts << " попыток!" << std::endl;
+		if (bulls == 4) {
+			std::cout << "Поздравляем! вы угадали число " << number << " за " << attempts << " попыток!" << std::endl;
 		}
 	}
 
