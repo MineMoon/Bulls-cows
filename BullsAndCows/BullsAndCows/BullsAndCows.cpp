f7df9f3327d9496b47aea2181a1462c2056be430
ilzwamh <ilzwamh@yandex.ru>
2024-12-14 14:53:56 +0500

    добавление ветки с выбором противника

--- BullsAndCows/BullsAndCows/BullsAndCows.cpp
+++ BullsAndCows/BullsAndCows/BullsAndCows.cpp
@@ -9,12 +9,51 @@
 int main() {
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
-	std::string number = Random();
+	int choice;
+	std::string number;
 	std::cout << "Добро пожаловать в игру 'Быки и коровы'!" << std::endl;
-	std::cout << "Компьютер загадал число из 4 уникальных цифр." << std::endl;
+	std::cout << "Компьютер или человек загадывает число из 4 уникальных цифр." << std::endl;
 	std::cout << "Ваша задача - угадать это число." << std::endl;
 	std::cout << "Введите ваше предположение, и вам сообщат количество быков и коров." << std::endl;
 	std::cout << "Быки - цифра на правильном месте, коровы - цифра есть, но на неправильном месте." << std::endl;
+	std::cout << std::endl;
+	std::cout << "Выберите противника: 1 - компьютер, 2 - второй человек: ";
+	std::cin >> choice;
+	while (choice < 1 || choice > 2) {
+		std::cout << "Неверно выбран противник, попробуйте еще раз: ";
+		std::cin >> choice;
+	}
+
+	if (choice == 1) {
+		number = Random();
+	}
+	else {
+		std::cout << "Загадайте число из 4 уникальных цифр: ";
+		std::cin >> number;
+		if (check_input(number) != 0) {
+			do {
+				switch (check_input(number))
+				{
+				case 1:
+					std::cout << "Число содержить только цифры от 0 до 9!\n";
+					break;
+				case 2:
+					std::cout << "Первой цифрой не может быть 0!\n";
+					break;
+				case 3:
+					std::cout << "Число должно быть 4-х значным, то есть состоять из 4 цифр!\n";
+					break;
+				case 4:
+					std::cout << "Все цифры в числе должны быть уникальны!\n";
+					break;
+				}
+				std::cout << "Введите ваше предположение еще раз: ";
+				std::cin >> number;
+			} while (check_input(number) != 0);
+		}
+	}
+	system("cls");
+	std::cout << "Число загадано!";
 
 	std::string guess;
 	int bulls = 0, cows = 0;
